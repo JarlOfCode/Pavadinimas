a6db2b507e6a65ea3b6013fd04171ba900197569
John <jonasbt007@gmail.com>
2020-11-17 16:29:36 +0200

    Player,Observer,Pattern

--- Testing/Player1Test.java
+++ Testing/Player1Test.java
@@ -2,93 +2,236 @@ package Testing;
 
 import static org.junit.Assert.*;
 
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.Test;
+
+import Strategy.Bullet;
+import edu.ktu.signalrclient.Player1;
 
 public class Player1Test {
 
 	@Test
 	public void testPlayer1() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		int tempHP = 3;
+		try {
+			if (testplayer.GetHP() < tempHP) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw e;
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
+	
+	//private int Pos_X = 10;
+	//private int Pos_Y = 15;
 	@Test
 	public void testMove() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		int x = 5;
+		int y = 5;
+		int tempPosx = 10;
+		int tempPosy = 15;
+		int tempResultX = x + tempPosx;
+		int tempResultY = y + tempPosy;
+
+		try {
+			try {
+				testplayer.move(x, y);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (tempResultX != testplayer.getX() || tempResultY != testplayer.getY()) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw e;
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testStartConstantObserve() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		testplayer.startConstantObserve();
+		//fail("Not yet implemented");
 	}
 
+	//private int Pos_X = 10;
+	//private int Pos_Y = 15;	
 	@Test
 	public void testGetX() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		int tempX = 10;
+		try {
+			if (testplayer.getX() != tempX) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw e;
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetY() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		int tempY = 15;
+		try {
+			if (testplayer.getY() != tempY) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw e;
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
+	//Needs loadimage()
 	@Test
 	public void testGetWidth() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		//int tempWidth = 10;
+		try {
+			if (testplayer.getWidth() == 0) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw e;
+		}
+		System.out.println("Test complete");
+		//fail("Not yet completed");
 	}
 
+	//Needs loadimage();
 	@Test
 	public void testGetHeight() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		//int tempWidth = 10;
+		try {
+			if (testplayer.getHeight() == 0) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw e;
+		}
+		System.out.println("Test complete");
+		//fail("Not yet completed");
 	}
 
 	@Test
 	public void testGetImage() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		testplayer.getImage();
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPlayerShoot() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		testplayer.playerShoot();
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
+	//Needs Key event
 	@Test
-	public void testKeyPressed() {
+	public void testKeyPressed() throws IOException {
+		int key = KeyEvent.VK_A;
+		//KeyEvent e = KeyEvent.VK_A;
+		Player1 testplayer = new Player1();
+		//KeyEvent e;
+		testplayer.keyPressed(key);
 		fail("Not yet implemented");
 	}
 
+	//Same here
 	@Test
 	public void testKeyReleased() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testNotifyObserver() {
-		fail("Not yet implemented");
+	public void testNotifyObserver() throws IOException {
+		Player1 testplayer = new Player1();
+		testplayer.notifyObserver();
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testUpdate() {
-		fail("Not yet implemented");
+		Player1 testplayer = new Player1();
+		testplayer.update();
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddHP() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		int temp = player.GetHP();
+		try {
+			//expected, real
+			player.AddHP(1);
+			assertEquals(temp+1,player.GetHP());
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		System.out.println("Test complete");
 	}
 
 	@Test
 	public void testSetMaxHP() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		int temp = 5;
+		try {
+			//expected, real
+			player.SetMaxHP(5);
+			assertEquals(temp,player.GetMaxHP());
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		System.out.println("Test complete");
 	}
 
 	@Test
 	public void testSetSpeed() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		int temp = 5;
+		try {
+			//expected, real
+			player.SetSpeed(5);
+			assertEquals(temp,player.GetSpeed());
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		System.out.println("Test complete");
 	}
 
 	@Test
 	public void testGetBullets() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		List<Bullet> templist = player.getBullets();
+		try {
+			if (templist == null) {
+				throw new AssertionError();
+			}			
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		//fail("Not yet implemented");
 	}
 
 }
--- Testing/PlayerTest.java
+++ Testing/PlayerTest.java
@@ -2,23 +2,56 @@ package Testing;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+
+import edu.ktu.signalrclient.Player1;
 
 public class PlayerTest {
 
 	@Test
 	public void testAddHP() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		int temp = player.GetHP();
+		try {
+			//expected, real
+			player.AddHP(1);
+			assertEquals(temp+1,player.GetHP());
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetMaxHP() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		int temp = 5;
+		try {
+			//expected, real
+			player.SetMaxHP(5);
+			assertEquals(temp,player.GetMaxHP());
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetSpeed() {
-		fail("Not yet implemented");
+		Player1 player = new Player1();
+		int temp = 5;
+		try {
+			//expected, real
+			player.SetSpeed(5);
+			assertEquals(temp,player.GetSpeed());
+		} catch (AssertionError e) {
+			throw(e);
+		}
+		System.out.println("Test complete");
+		//fail("Not yet implemented");
 	}
 
 }
